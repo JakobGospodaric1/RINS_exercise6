77a73c1f25bedbb8887e9cdf9d7f25428a757d54
JakobGospodaric1 <jg9837@student.uni-lj.si>
2023-04-28 14:36:05 +0200

    dela zaznavanje
    Co-authored-by: Kevin <kevP0@users.noreply.github.com>

--- src/program.py
+++ src/program.py
@@ -3,18 +3,49 @@
 import rospy
 
 # import the PointCloud2 message
-from sensor_msgs.msg import PointCloud2
+from sensor_msgs.msg import PointCloud2, Image
 
-def callback_cylinder(msg):
-    # Print the point cloud
-    rospy.loginfo(msg.row_step)
+import message_filters
+
+# Import OpenCV libraries and tools
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+
+# Initialize the CvBridge class
+bridge = CvBridge()
+
+cv2.startWindowThread()
+
+def find_rings_callback(rgb_msg, depth_msg):
+    #print timestamp
+    print(rgb_msg.header.stamp)
+
+    try:
+        rgb_image = bridge.imgmsg_to_cv2(rgb_msg, "passthrough")
+    except CvBridgeError as e:
+        print(e)
+
+    try:
+        depth_image = bridge.imgmsg_to_cv2(depth_msg, "passthrough")
+    except CvBridgeError as e:
+        print(e)
+
+    if depth_image is None or rgb_image is None:
+        return
+    
+    cv2.imshow("RGB Image", rgb_image)
+    cv2.waitKey(50)
 
 if __name__ == '__main__':
     # Initialize the ROS node
     rospy.init_node('program', anonymous=True)
 
-    # Create a ROS subscriber with the desired topic and message type
-    sub = rospy.Subscriber("/cylinder", PointCloud2, callback_cylinder)
+    # Message filering for synchronizing the rgb and depth images
+    rgm_sub = message_filters.Subscriber("/camera/rgb/image_raw", Image)
+    depth_sub = message_filters.Subscriber("/camera/depth/image_raw", Image)
+
+    synchronizer = message_filters.ApproximateTimeSynchronizer([rgm_sub, depth_sub], 10, 0.1)
+    synchronizer.registerCallback(find_rings_callback)
 
     # Spin
     rospy.spin()
